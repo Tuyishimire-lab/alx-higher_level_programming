1da682ccb17048826d6289b29c48fc5cddd551f7
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-09-07 12:34:26 +0200

    Create 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(1,10):
+  for j in rang(i+1, 10):
+    if (i is not 8) and (j is not 9):
+      print("{}{}, ".format(i, j),end="")
+    else:
+      print("{}{}".format(i, j))
