fba04b8e9098a0184c0ca263b0dc3d6b0347cf80
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-09-09 15:03:49 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python3.4
-if __name__ == "__main__":
+#!/usr/bin/python3
+
+
+if __name__ == '__main__':
     import hidden_4
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print(i)
+
+    name_list = dir(hidden_4)
+    for st in name_list:
+        if not st.startswith('__'):
+            print('{:s}'.format(st))
