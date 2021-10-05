23ac03aa651e009fef86580bf5d1208bc535486b
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-10-05 18:28:49 +0200

    Correction

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""blocked class module"""
+
+
 class LockedClass:
+    """object prevents dynamic attribute"""
 
     __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
