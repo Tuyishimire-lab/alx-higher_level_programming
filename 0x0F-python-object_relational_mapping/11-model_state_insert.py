9c27f413a5a30903c39ea51215242792c5832e9a
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-12-24 16:48:08 +0200

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""
-update state: given id, change state name
-parameters given to script: username, password, database
-"""
-
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+"""adds the State object “Louisiana”
+to the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
 
-    # make engine for database
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    # add new state and commit to table
-    new = State(name="Louisiana")
+    session = Session(engine)
+    new = State(name='Louisiana')
     session.add(new)
+    new_state = session.query(State).filter(State.name == 'Louisiana').first()
     session.commit()
-
-    print("{:d}".format(new.id))
-
+    print("{}".format(new_state.id))
     session.close()
-
